31c707ef158d147bd480eb4935e4c79ccd4d23b8
Sergei Martao <sergei.martao@gmail.com>
2018-08-17 07:52:16 -0300

    task 2 gerando arquivo aleatorio

--- lfcs2018.sh
+++ lfcs2018.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
 ### Varivais ###
-DIR=/tmp
+DIR1="/tmp"
+DIR2="$DIR1/files"
 
 ### Essential Commands ###
 # task 1
 # Gerando IP randomico
-unset IP; for i in `seq 4`; do IP=$IP.$(od -An -N1 -i /dev/random); done ; echo $IP |  cut -d. -f2-5 | sed 's/[[:blank:]]//g' > /$DIR/ipssh
-unset IP; for i in `seq 4`; do IP=$IP.$(od -An -N1 -i /dev/random); done ; echo $IP |  cut -d. -f2-5 | sed 's/[[:blank:]]//g' >> /$DIR/ipssh
+unset IP; for i in `seq 4`; do IP=$IP.$(od -An -N1 -i /dev/random); done ; echo $IP |  cut -d. -f2-5 | sed 's/[[:blank:]]//g' > $DIR1/ipssh
+unset IP; for i in `seq 4`; do IP=$IP.$(od -An -N1 -i /dev/random); done ; echo $IP |  cut -d. -f2-5 | sed 's/[[:blank:]]//g' >> $DIR1/ipssh
 
 # task 2
-mkdir $DIR/xpto2 2> /dev/null
-touch $DIR/xpto.jpg
-touch $DIR/xpto.png
-touch $DIR/zfff.xtx
-touch $DIR/zfff.XTX
+# Criando arquivos e diretorios
+for i in {1..100}; do mktemp -dqp $DIR2 >/dev/null; for i in {1..20};do mktemp -qp $DIR2 > /dev/null ;done; done
 
 # task 4
-touch $DIR/axxx.txt
+#touch $DIR/axxx.txt
 
 # task 5
 echo "#!/bin/bash
 echo 'saida normal'
-ocorreu um erro" > /tmp/scriptstd.sh
-chmod 755 /tmp/scriptstd.sh
+ocorreu um erro" > $DIR1/scriptstd.sh
+chmod 755 $DIR1/scriptstd.sh
 
 # task 6
 
